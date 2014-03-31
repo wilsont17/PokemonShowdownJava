930c5ce6549d66e9e3f0a9f18873701bc78c9916
samisftw <sginzbu@yahoo.com>
2014-03-31 19:24:35 -0400

    Adds do nothing method

--- src/RunThis.java
+++ src/RunThis.java
@@ -1,10 +1,14 @@
 
 public class RunThis 
 {
-
 	public static void main(String[] args) 
 	{
 		System.out.println("this is a test");
+	}
+	
+	public static void doNothing()
+	{
+		//does nothing
 	}
 
 }
