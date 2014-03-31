5db9aff4496d1d0d9a1135bdb16c4482ef24f22d
samisftw <sginzbu@yahoo.com>
2014-03-31 19:32:07 -0400

    pokemon class added

---
+++ src/Pokemon.java
@@ -0,0 +1,29 @@
+
+public class Pokemon 
+{
+	private int HP;
+	private int attack;
+	private int defense;
+	private int spAttack;
+	private int spDefense;
+	private int speed;
+	
+	private String name;
+	
+	Pokemon(String name, int hp, int attack, int defense, int spAttack, int spDefense, int speed)
+	{
+		
+		this.name = name;
+		this.HP = hp;
+		this.attack = attack;
+		this.defense = defense;
+		this.spAttack = spAttack;
+		this.spDefense = spDefense;
+		this.speed = speed;
+	}
+	
+	
+	
+	
+
+}
