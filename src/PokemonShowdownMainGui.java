0ebb8eb501bd69839888d38df1c2c310e224f938
Shadow420blazeit <lang.gao683@gmail.com>
2014-03-31 19:40:35 -0400

    arraylists not working

--- src/PokemonShowdownMainGui.java
+++ src/PokemonShowdownMainGui.java
@@ -1,5 +1,32 @@
+import java.util.*;
+import javax.swing.*;
 
 public class PokemonShowdownMainGui 
 {
+	ArrayList<Pokemon> p1pokemon = new ArrayList<Pokemon>();
+	ArrayList<Pokemon> p2pokemon = new ArrayList<Pokemon>();
 	
+	
+	
+	
+	
+	
+	
+	//Mechanics
+	
+	if (!p1switch && !p2switch)
+	{
+		if (p1pokemon.getSpeed() > p2pokemon.getSpeed())
+		{
+			
+		}
+		else if (p1pokemon.getSpeed() == p2pokemon.getSpeed())
+		{
+			
+		}
+		else
+		{
+			
+		}
+	}
 }
