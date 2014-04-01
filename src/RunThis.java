348dc744f86b066dd6a842142948cfa7c758f773
samisftw <sginzbu@yahoo.com>
2014-03-31 21:05:47 -0400

    now creates GUI thread

--- src/RunThis.java
+++ src/RunThis.java
@@ -1,14 +1,15 @@
+import javax.swing.*;
 
 public class RunThis 
 {
+	
 	public static void main(String[] args) 
 	{
-		System.out.println("this is a test");
-	}
-	
-	public static void doNothing()
-	{
-		//does nothing
-	}
+		SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new PokemonShowdownMainGui();
+            }
+        });
+	}	
 
 }
