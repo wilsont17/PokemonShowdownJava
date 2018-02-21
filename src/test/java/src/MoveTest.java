3d82eed24652fdad936cf99ee0bb45e927f82d64
Tyler Wilson <wilsont17@nku.edu>
2018-02-21 01:49:13 -0500

    Get Effect Chance
    Get effect chance now works; tests written for it

---
+++ src/test/java/src/MoveTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import org.junit.Assert;
+
+public class MoveTest {
+    @Test
+    public void create_move_test() {
+        Move move = new Move();
+    }
+
+    @Test
+    public void get_move_effect_chance_test() {
+        Move move = new Move();
+        move.setEffectChance(50);
+        Assert.assertEquals(move.getEffectChance(), 50);
+    }
+}
