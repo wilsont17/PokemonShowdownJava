f74974340295ee61a41b8392263926c0095759bb
Beannsss <bpwhelan95@gmail.com>
2018-03-28 22:00:37 -0400

    Add files via upload

--- src/test/java/src/PokemonShowdownMainGuiTest.java
+++ src/test/java/src/PokemonShowdownMainGuiTest.java
@@ -1,18 +1,30 @@
-import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.FileNotFoundException;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+import org.junit.Assert.*;
 
 public class PokemonShowdownMainGuiTest {
 
     @Test
-    public void stabResult_test(){
-        Move move = new Move();
-        move.setDmgType(10);
-        Pokemon pokemon = new Pokemon();
-        pokemon.addType("fire");
-        PokemonShowdownMainGui gui = new PokemonShowdownMainGui("test");
-        Assert.assertEquals(1.5, gui.stabResult(pokemon, move), .1);
+    public void
+    givenNonRandom1v1_whenGivenPokemonIndexOf24_PokemonIsPikachu() throws AWTException {
+        Robot robot = new Robot();
+        RunThis.main(null);
+        robot.delay(5000);
+        robot.keyPress(KeyEvent.VK_TAB);
+        robot.keyRelease(KeyEvent.VK_TAB);
+        robot.delay(1000);
+        robot.keyPress(KeyEvent.VK_TAB);
+        robot.keyRelease(KeyEvent.VK_TAB);
+        robot.delay(1000);
+        robot.keyPress(KeyEvent.VK_1);
+        robot.keyRelease(KeyEvent.VK_1);
+        robot.delay(1000);
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+        robot.delay(5000);
+        Assert.assertEquals("pikachu", RunThis.gui.p1Pokemon.get(0).getName());
     }
-
 }
